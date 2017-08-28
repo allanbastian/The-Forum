6374217f95ce7dfa7282bdc480b97921bcb78534
Allan Bastian <allan.batian9.ab@gmail.com>
2017-08-28 17:43:23 +0530

    Added UX

--- app/src/main/java/com/adgvit/allan/theforum/MainActivity.java
+++ app/src/main/java/com/adgvit/allan/theforum/MainActivity.java
@@ -1,13 +1,38 @@
 package com.adgvit.allan.theforum;
 
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.FileProvider;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private CardView userCard;
+    private EditText doubt;
+    private Button Send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialization
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("FAQ/Forum");
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        FragmentPagerAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        userCard = (CardView) findViewById(R.id.userCard);
+        doubt = (EditText) findViewById(R.id.doubt);
+        Send = (Button) findViewById(R.id.send);
     }
 }
